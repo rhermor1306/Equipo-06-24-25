03e209ca3da9336e1c446cf994521dc37f362596
Marta-Molina <martamolinaescalona@hotmail.com>
2025-01-20 10:29:06 +0100

    Div5

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,7 +2,22 @@ package Main;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Proyecto del equipo 06");
-	}
+		//System.out.println("Proyecto del equipo 06");
+		
+		Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Introduce un número: ");
+        int numero = scanner.nextInt();
+        
+        boolean resultado = esDivisiblePorCinco(numero);
+        
+        System.out.printf("¿El número %d es divisible por 5? %b%n", numero, resultado);
+
+        scanner.close();
+    }
+
+    public static boolean esDivisiblePorCinco(int numero) {
+        return numero % 5 == 0;
+    }
 
 }
