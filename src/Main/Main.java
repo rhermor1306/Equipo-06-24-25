3bcc6d47ddfa2cc5a96b8cab41cbe8108ee80065
rhermor1306 <rafaelherediamoreno34@gmail.com>
2025-01-24 14:20:00 +0100

    final de proyecto rafa

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,19 +1,35 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		//System.out.println("Proyecto del equipo 06");
+		System.out.println("Proyecto del equipo 06");
 		
 		Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Introduce un número: ");
-        int numero = scanner.nextInt();
-        
-        boolean resultado = esDivisiblePorCinco(numero);
-        
-        System.out.printf("¿El número %d es divisible por 5? %b%n", numero, resultado);
-
-        scanner.close();
+        int opcion;
+        do {
+            System.out.println("Menu de opciones:");
+            System.out.println("1.-Verificar si un numero es par:");
+            System.out.println("2.-Verificar si es divisble entre tres :");
+            System.out.println("3.-Verificar si es divisible entre cinco :");
+            System.out.println("4.- Salir");
+            System.out.println("Seleccione una opción");
+            opcion = scanner.nextInt();
+            
+            if (opcion >= 1 && opcion <= 3) {
+                System.out.print("Ingrese un número:");
+                int numero = scanner.nextInt();
+                switch (opcion) {
+                    case 1: System.out.println("¿Es par? " + esPar(numero));
+                        break;
+                    case 2: System.out.println("Es divisible por tres?" + esDivisiblePorTres(numero));
+                        break;
+                    case 3: System.out.println("Es divisible por Cinco?" + esDivisiblePorCinco(numero));
+                        break;  
+                }
+            }
+        } while (opcion != 4);
     }
 
     public static boolean esDivisiblePorCinco(int numero) {
@@ -26,9 +42,6 @@ public class Main {
 		}
 		return false;
 	}
-
-
-
 
 	public static boolean esDivisiblePorTres(int numero){
 		return numero % 3 == 0;
--- src/Main/MainTest.java
+++ src/Main/MainTest.java
@@ -1,15 +1,28 @@
 
 package Main;
 
+import java.util.Scanner;
+
 public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("3 es divisible entre 3 " + Main.esDivisiblePorTres(3));
 		System.out.println("4 es divisible entre 3 " + Main.esDivisiblePorTres(4));
-		System.out.println("6 es divisible entre 3 " + Main.esDivisiblePorTres(6));	
+		System.out.println("6 es divisible entre 3 " + Main.esDivisiblePorTres(6));
+		
+		System.out.println("El 2 es par: " + Main.esPar(2));
+
+		Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Introduce un número: ");
+        int numero = scanner.nextInt();
+        
+        boolean resultado = Main.esDivisiblePorCinco(numero);
+        
+        System.out.printf("¿El número %d es divisible por 5? %b%n", numero, resultado);
+
+        scanner.close();
 	}
 
-	boolean metodo = Main.esPar(2);
-	System.out.println("El 2 es par: " + metodo);
-	
+
 
 }
